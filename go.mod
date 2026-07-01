4cf5e717649d58c45b25199059983b081a7f380d
thingammabob <86408122+thingammabob@users.noreply.github.com>
2026-07-02 00:20:43 +0530

    Add http server for produce and consume logs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thingammabob/proglog
 
 go 1.26.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- internal/server/log.go
+++ internal/server/log.go
@@ -7,7 +7,7 @@ import (
 
 type Record struct {
 	Value  []byte `json:"value"`
-	Offset uint64 `json:"offset`
+	Offset uint64 `json:"offset"`
 }
 type Log struct {
 	mu      sync.Mutex
@@ -24,6 +24,7 @@ func (c *Log) Append(record Record) (uint64, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	record.Offset = uint64(len(c.records))
+	c.records = append(c.records, record)
 	return record.Offset, nil
 }
 func (c *Log) Read(offset uint64) (Record, error) {
